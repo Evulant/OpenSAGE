8d8417c6d87920871aadec1132139c138179f086
Michael Schnabel <michael-schnabel@gmx.net>
2020-08-18 00:04:01 +0200

    added some comments

--- src/OpenSage.Game/Logic/Object/GameObject.cs
+++ src/OpenSage.Game/Logic/Object/GameObject.cs
@@ -197,8 +197,6 @@ namespace OpenSage.Logic.Object
 
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
-        public GameObject Garrison;
-
         internal GameObject(
             ObjectDefinition objectDefinition,
             GameContext gameContext,
